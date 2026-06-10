3a75592f1b1070c3e61af331cc76eaa81811dceb
Tino5520 <valentinoluca@hotmail.com>
2026-06-10 16:55:43 -0300

    Cart: Model, Dtos, Exceptions

---
+++ Cart.API/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿namespace Cart.API.Models
+{
+    public class Cart
+    {
+        public Guid UsuarioId { get; set; }
+        public List<CartItem> Items { get; set; } = new();
+        public DateTime FechaActualizacion { get; set; }
+    }
+
+    public class CartItem
+    {
+        public Guid UsuarioId { get; set; }
+        public Guid ProductoId { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
